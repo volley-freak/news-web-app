6e7b7fed5ac4dfd85004e1f52901ffa8ec7f38c8
Volley-Freak <hrithikgusain07@gmail.com>
2022-03-11 14:05:34 +0530

    Updates

---
+++ precache-manifest.e2dabefe367b3dbef7cf92f68b2e8297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af80dff9060d1bd24b53",
+    "url": "/news-web-app/static/css/main.d94e1884.chunk.css"
+  },
+  {
+    "revision": "af80dff9060d1bd24b53",
+    "url": "/news-web-app/static/js/main.af80dff9.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/news-web-app/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "37bc531a5e86a30a5236",
+    "url": "/news-web-app/static/js/2.37bc531a.chunk.js"
+  },
+  {
+    "revision": "a53dfbcf56563ed6e77a",
+    "url": "/news-web-app/static/js/runtime~main.a53dfbcf.js"
+  },
+  {
+    "revision": "17af93d5b5d60dc0ba9d010e335bf844",
+    "url": "/news-web-app/index.html"
+  }
+];
